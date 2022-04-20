bf04329c11352786482c29552bc54cebe5648e93
MonJ0J <98435698+MonJ0J@users.noreply.github.com>
2022-04-20 13:32:33 -0400

    Update Decision_tree.py

--- Decision_tree.py
+++ Decision_tree.py
@@ -86,20 +86,31 @@ def get_root(features):
     node = Node(features, None, None )
     return node
 
-def build_decision_tree(curr_list, parent, goal):
-    lb = list(curr_list[0].keys())[:-1]
-    table = convert(curr_list, gl, lb)
+def build_decision_tree(curr_list, parent, goal): 
+    stop = True
+    for item in curr_list:
+        if len(item)>1:
+            stop = False
+    print()
+    if stop == True:
+        return parent
+    lb = list(curr_list[0].keys())[:-1]#size, color...
+    table = convert(curr_list, gl, lb)#size{tiny:[yes, no]...} 
     entropy, attribute = minimum_entropy(table)
     if entropy == 0.0:
         return
     for key in table[attribute]:
         temp_list = []
         for d in curr_list:
-            if d[attribute] == key:
-                temp_list.append(d)
-        node = Node (temp_list, attribute, None)
-        print(node.get_subdict())
-        print()
+            if d[attribute] == key:#attr = size, color...
+                temp_dic = copy.deepcopy(d)
+                temp_dic.pop(attribute)
+                temp_list.append(temp_dic)
+                
+        node = Node (temp_list, key, goal)
+        print(node.get_subdict(),node.get_attr())
+        
+        build_decision_tree(temp_list,node,goal)
 
 tb, lb = read_file('pets.txt')
 gl = return_goal(tb)
